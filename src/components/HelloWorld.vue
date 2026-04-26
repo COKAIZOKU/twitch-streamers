ba89d23ca823581b435e47fe2930a96a1b3dc232
pvvegar <proteicode@proton.me>
2026-04-26 02:33:38 -0400

    style: added inter font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const description =
   <div class="flex bg-purple-twitch min-h-screen w-screen">
     <div class="bg-white mx-auto mt-60 h-160 w-150 p-12">
       <div class="flex flex-col justify-between h-full">
-        <div class="flex gap-5 justify-center text-gray-500 font mb-10">
+        <div class="flex gap-6 justify-center text-gray-500 font mb-10">
           <a>All</a>
           <a>Offline</a>
           <a>Online</a>
@@ -22,8 +22,8 @@ const description =
               <p class="text-md">username</p>
               <p class="text-sm text-gray-400">
                 {{
-                  description.length > 65
-                    ? description.slice(0, 65) + '...'
+                  description.length > 60
+                    ? description.slice(0, 60) + '...'
                     : description
                 }}
               </p>
@@ -31,7 +31,9 @@ const description =
           </div>
         </div>
         <div class="flex justify-center">
-          <img :src="logo" alt="Icon" class="w-5" />
+          <a href="https://twitch.tv/" target="_blank" rel="noopener noreferrer">
+            <img :src="logo" alt="Icon" class="w-5" />
+          </a>
         </div>
       </div>
     </div>
