e2018dc54f0e9038e0dcd345b52bc4c22a066806
pvvegar <proteicode@proton.me>
2026-04-26 00:58:35 -0400

    feat: added skeleton of user + username dummy + title dummy + avatar dummy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
+const description =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 </script>
+
 
 <template>
   <div class="flex bg-purple-twitch min-h-screen w-screen">
-    <div class="bg-white mx-auto mt-60 h-160 w-150 p-10">
-      <div class="flex gap-5 justify-center text-gray-400 font ">
+    <div class="bg-white mx-auto mt-60 h-160 w-150 p-12">
+      <div class="flex gap-5 justify-center text-gray-500 font mb-10">
         <a>All</a>
         <a>Offline</a>
         <a>Online</a>
       </div>
-
+      <div class="flex gap-6">
+        <img src="/avatar.png" class="w-15 h-15 rounded-full object-cover" />
+        <div class="flex flex-col justify-center">
+          <p class="text-md">username</p>
+          <p class="text-sm text-gray-400">
+            {{
+              description.length > 65
+                ? description.slice(0, 65) + '...'
+                : description
+            }}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
